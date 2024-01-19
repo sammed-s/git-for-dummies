960067c7b6ae625c3894cf656dcc26d0ed7cd935
sammed-s <sammed.s@drillbitplagiarism.com>
2024-01-19 22:23:25 +0530

    karna updated

--- karna.js
+++ karna.js
@@ -1,3 +1,5 @@
 console.log('====================================');
 console.log("captain karna strikes");
 console.log('====================================');
+console.log("captain karna strikes again");
+console.log('====================================');
